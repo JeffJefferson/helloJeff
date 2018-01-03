1a4097b05e70eb5e6f751b23a230ce9a162ec316
JeffJefferson <karsten.giebels@gmail.com>
2018-01-03 16:30:56 +0100

    added Solution
    added Solution

---
+++ SolutionName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jeff!");
+            Console.ReadLine();
+        }
+    }
+}
